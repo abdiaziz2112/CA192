3a626a9743401b78a4b3426ca1c92a670b0e2ed0
Abdiaziz Ahmed <84683112+abdiaziz2112@users.noreply.github.com>
2022-01-07 21:30:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-function demoA(){ alert( "Wax assignment ah ma jiraan saaxiib sorry!!."); }
+//function demoA(){ alert( "Wax assignment ah ma jiraan saaxiib sorry!!."); }
+
+function toggle(){
+ 
+ let toggle = document.querySelector('.popup-container')
+
+ toggle.classList.toggle('toggle');
+
+}
+
